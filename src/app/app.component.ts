1d3e881160f72790b0e872c42963170c68d5364b
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-10-25 19:25:42 -0600

    theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { OverlayContainer} from '@angular/cdk/overlay';
+import * as $ from 'jquery';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'bmangav7';
+export class AppComponent implements OnInit {
+    constructor(
+        public overlayContainer: OverlayContainer
+    ){}
+    @HostBinding('class') componentCssClass;
+
+    public changeTheme: boolean = false;
+
+    eventChangeTheme(event: any): void{
+        let theme = this.changeTheme ? 'dark-theme' : 'light-theme';
+        this.overlayContainer.getContainerElement().classList.add(theme);
+        this.componentCssClass = theme;
+        this.setLetterColor(theme);
+    }
+    ngOnInit(): void{
+        let theme = 'light-theme';
+        this.overlayContainer.getContainerElement().classList.add(theme);
+        this.componentCssClass = theme;
+        this.setLetterColor(theme);
+    }
+    setLetterColor(theme: string): void{
+        setTimeout(() => {
+            switch(theme){
+                case "light-theme":{
+                    $(".title-static").css("color", "#363636");
+                    break;
+                }
+                case "dark-theme":{
+                    $(".title-static").css("color", "white");
+                    break;
+                }
+            }
+        }, 1);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+
+export function Token(): string {
+    return localStorage.getItem('token');
+}
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        JwtModule.forRoot({
+            config: {
+                tokenGetter: Token,
+                whitelistedDomains: [
+                    'localhost:80'
+                ],
+                skipWhenExpired: true
+            }
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
